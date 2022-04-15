f559ab9589b1956ddcf30b8b7559cad0435bbfd6
unknown <mr.shashidhar30@gmail.com>
2022-04-15 11:08:08 +0530

    cart dropdown cart item cart total number in icon

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,15 +1,26 @@
+import { useContext } from 'react'
+
+import { CartContext } from '../../context/cart.context'
+
 import Button from '../button/button.component'
+import CartItem from '../cart-item/cart-item.component'
 
 import './cart-dropdown.styles.scss'
 
 const CartDropdown = () => {
+
+  const {cartItems} = useContext(CartContext);
     return (
         < div className='cart-dropdown-container'>
 
-        <div className='cart-item' />
+        <div className='cart-items'>
+      { cartItems.map((item) => (<CartItem key={item.id } cartItem={item} />))}
+
+        </div>
         <Button>Go To Checkout</Button>
         </div>
     )
 }
 
 export default CartDropdown;
+
